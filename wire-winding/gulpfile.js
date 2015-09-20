2b816a581f8d9b6c58297b6d9ad06884a5e1a1e0
Eric Schreffler <omnicrola+tards@gmail.com>
2015-09-19 23:45:50 -0400

    Added bare Polymer framework and a test element

--- wire-winding/gulpfile.js
+++ wire-winding/gulpfile.js
@@ -1,22 +1,39 @@
 var gulp = require('gulp');
 var vulcanize = require('gulp-vulcanize');
 var concat = require('gulp-concat');
+var useref = require('gulp-useref');
 
-gulp.task('default', function()
-);
+var outputFolder = './dist';
+gulp.task('default', ['copy-index', 'vulcanize', 'scripts', 'copy-webcomponents']);
 
 gulp.task('vulcanize', function () {
-    return gulp.src('src/index.html')
+    return gulp.src([
+        'src/index.html'
+    ])
         .pipe(vulcanize({
             abspath: '',
             excludes: [],
             stripExcludes: false
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(outputFolder));
 });
 
-gulp.task('scripts', function(){
+gulp.task('copy-webcomponents', function () {
+    return gulp.src([
+        'bower_components/webcomponentsjs/webcomponents-lite.min.js',
+        'bower_components/polymer/polymer.html',
+        'bower_components/platform/platform.js'
+    ])
+        .pipe(gulp.dest(outputFolder + '/components'));
+});
+
+gulp.task('copy-index', function () {
+    return gulp.src('./index.html')
+        .pipe(gulp.dest(outputFolder));
+});
+
+gulp.task('scripts', function () {
     return gulp.src('src/js/*.js')
         .pipe(concat('main.js'))
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest(outputFolder + '/js'));
 });
